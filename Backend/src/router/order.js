8dec7ad7aa1fa6cb52095a2be0c86f65db855350
itmrrobot <nhannguyentri05@gmail.com>
2024-04-26 16:49:38 +0700

    Add verify token to get orders by admin

--- Backend/src/router/order.js
+++ Backend/src/router/order.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const upload = require('../common/upload');
 const orderController = require('../controllers/orderController');
 const {isAdmin} = require('../middleware/verifyRole');
+const {verifyToken} = require('../middleware/verifyToken');
 
 router.get('/order/:id',orderController.handleGetOrderById)
-.get('/orders',isAdmin,orderController.handleGetAllOrder)
+.get('/orders',verifyToken,isAdmin,orderController.handleGetAllOrder)
 .get('/orders/:id',orderController.handleGetOrderList)
 .post('/order/create',orderController.handleCreateNewOrder)
 .put('/order/update/:id',orderController.handleUpdateOrder)
