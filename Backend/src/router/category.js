db3c781265c90fb86d837ce187de85d86665344a
itmrrobot <nhannguyentri05@gmail.com>
2024-05-01 22:40:44 +0700

    Change logic for update category api

--- Backend/src/router/category.js
+++ Backend/src/router/category.js
@@ -6,7 +6,7 @@ const {upload} = require('../common/upload');
 router.get('/category',categoryController.handleGetCategoryList)
 .get('/category/:id',categoryController.handleGetCategoryById)
 .post('/category/create',upload.single('img'),categoryController.handleCreateNewCategory)
-.put('/category/update/:id',categoryController.handleUpdateCategory)
+.put('/category/update/:id',upload.single('img'),categoryController.handleUpdateCategory)
 .delete('/category/delete/:id',categoryController.handleDeleteCategory)
 
 module.exports = router;
