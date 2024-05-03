d584a529a0dba9a347ad94f23d7f51cf1064a6af
itmrrobot <nhannguyentri05@gmail.com>
2024-05-03 10:38:35 +0700

    Add logic to recommend controller

--- Backend/src/controllers/recommendController.js
+++ Backend/src/controllers/recommendController.js
@@ -3,7 +3,7 @@ const recommendService = require("../service/recommendService");
 const handleRecommmendKeyWordProduct = async(req,res) => {
     try {
         const recommendList = await recommendService.getRecommendListProduct(req.body.search);
-        if(recommendList?.recommendations.length===0||recommendList===null) return res.status(404).send();
+        if(recommendList?.recommendations.length===0||recommendList===null) return res.status(404).send([]);
         return res.status(200).send(recommendList);
     } catch(e) {
         console.log(e);
