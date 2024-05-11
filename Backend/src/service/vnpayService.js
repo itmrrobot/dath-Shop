195232989b69232ec2a8ca9f45bfc3ba053ccc4b
itmrrobot <nhannguyentri05@gmail.com>
2024-05-11 22:43:56 +0700

    Change vnpay logic

--- Backend/src/service/vnpayService.js
+++ Backend/src/service/vnpayService.js
@@ -75,4 +75,20 @@ const getPaymentSuccess = async (res) => {
           res.redirect("http://localhost:3000/vnpay/payment/success");
   };
   
+  function sortObject(obj) {
+	let sorted = {};
+	let str = [];
+	let key;
+	for (key in obj){
+		if (obj.hasOwnProperty(key)) {
+		str.push(encodeURIComponent(key));
+		}
+	}
+	str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+    }
+    return sorted;
+}
+
   module.exports = { createPayment, getPaymentSuccess };
